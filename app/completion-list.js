52a995d00935c5aee0a4c94c9384d8e0547af949
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-11-03 00:50:52 +0100

    Implement completion selection

--- app/completion-list.js
+++ app/completion-list.js
@@ -5,16 +5,18 @@ export default class CompletionList extends React.Component {
   render () {
     return (
       <ul className="completion-list">
-      {this.props.items.map(completionItem)}
+      {this.props.items.map(completionItem.bind(null, this.props.selected))}
       </ul>
     );
   }
 }
 
 
-function completionItem (item) {
+function completionItem (selected, item, i, items) {
+  var selectedClass = (selected % items.length == i) ? ' selected' : '';
+
   return (
-    <li key={item.id} className="completion-item">
+    <li key={item.id} className={'completion-item' + selectedClass }>
     <h2 className="completion-item-label">{item.match}</h2>
     {item.description ? <p className="completion-item-description">{item.description}</p> : null}
     </li>
--- app/completion.js
+++ app/completion.js
@@ -17,10 +17,12 @@ export default class Completion extends React.Component {
 
     wikiData.getMatches(props.token.type, search).then(function (items) {
       this.setState({items: items});
+      this.props.onSelect(items[this.props.selected % items.length])
+
     }.bind(this));
   }
 
   render () {
-    return <CompletionList items={this.state.items} />;
+    return <CompletionList items={this.state.items} selected={this.props.selected}/>;
   }
 }
