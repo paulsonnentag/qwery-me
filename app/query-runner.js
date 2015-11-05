60e76bd2ad3be2a2d500a124175d2f5e94b01af7
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-11-05 19:23:26 +0100

    Add result table

--- app/query-runner.js
+++ app/query-runner.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import getQuery from './get-query';
 import wikiData from './wiki-data';
+import ResultTable from './result-table';
 
 export default class QueryRunner extends React.Component {
 
   constructor () {
     super();
-    this.state = {query: {}}
+    this.state = {result: null}
   }
 
   runQuery () {
     var query = getQuery(this.props.cm);
 
-    wikiData.query(query).then(response => {
-      this.setState({query: response});
-    });
+    wikiData.query(query)
+      .then(result => this.setState({result}));
   }
 
   render () {
     return (
       <div className="query-runner">
       <button onClick={this.runQuery.bind(this)}>run</button>
-      {JSON.stringify(this.state.query)}
+      <ResultTable result={this.state.result}/>
       </div>
     );
   }
