a5c281ebfcfd0fb119d06bfe8d3cae86dc05a9ea
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-11-08 23:08:14 +0000

    Replace query editor with statement editor skeleton

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import QueryEditor from './query-editor.js';
+import StatementEditor from './statement-editor.js';
 
 export default class App extends React.Component {
   render () {
@@ -8,7 +9,7 @@ export default class App extends React.Component {
       <header className="header">
       <h1 className="header-title">qwery.me</h1>
       </header>
-      <QueryEditor />
+      <StatementEditor/>
       </div>
     );
   }
---
+++ app/statement-editor.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class StatementEditor extends React.Component {
+
+  constructor () {
+    super();
+
+    this.state = {statements: []};
+  }
+
+  render () {
+    return <h1>Statement Editor</h1>;
+  }
+}
